9347dae38fec8451877fb1d3f9fc5faedf08ab7d
João Vitor Farias <jvmnc232@gmail.com>
2024-03-05 11:14:31 -0300

    componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+import logoCoworking from "../../assets/logo.png"
+
+
+const Header = () => {
+    return (
+        <Card className="bg-gray-700" >
+            <CardContent className="p-5 flex justify-between items-center flex-row">
+                <a href="/" >
+                    <img src={logoCoworking} alt="" className="w-[120px]" />
+                </a>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon" className="h-8 w-8">
+                            <MenuIcon size={16} />
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent className="p-0">
+
+                    </SheetContent>
+                </Sheet>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Header;
