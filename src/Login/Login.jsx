50c5da81fe86a6c5715a20164d6c00d3ff8cbb9f
Hitalo Albuquerque <hitalohpa@gmail.com>
2024-03-05 15:58:11 -0300

    Autenticação de login e mensagens de erro

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,25 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./login.css";
 import fotoCoworking from "../assets/coworking.png";
-import { useState } from "react";
-import { Link } from 'react-router-dom';
 import logo from "../assets/logo.png";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from "axios";
 
 const Login = () => {
   const [modal, setModal] = useState(false);
   const [input, setInput] = useState("");
-  const [login, setLogin] = useState("")
-  const [senha, setSenha] = useState("")
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
 
   const submitLogin = async () => {
-    const response = await axios.get("http://localhost:3000/usuarios")
-    if(login === response.data[1].email && senha === response.data[1].password ) {
-        window.location.href = '/home';
+    try {
+      const response = await axios.get("http://localhost:3000/usuarios");
+      if (login === response.data[1].email && senha === response.data[1].password) {
+        window.location.href = "/home";
+      } else if (login === '' || senha === '') {
+        toast.error("Você precisa preencher todos os campos!");
+      } else {
+        toast.error("Usuário ou senha incorretos!");
+        setLogin('')
+        setSenha('')
+      }
+    } catch (error) {
+      console.error(error)
     }
-
-  }
+  };
 
   const handleModal = () => {
     setModal(!modal);
@@ -27,13 +36,12 @@ const Login = () => {
   };
 
   const handlePassword = () => {
-    if (input === "" || !input.includes("@")  || !input.includes(".com") ) {
+    if (input === "" || !input.includes("@") || !input.includes(".com")) {
       toast.error("Digite um email válido!");
-      setInput("");
     } else {
       toast.success("Email enviado com sucesso, verifique sua caixa de entrada! ✅");
       setInput("");
-      setModal(false)
+      setModal(false);
     }
   };
 
@@ -56,22 +64,23 @@ const Login = () => {
         <form className="formulario-login" onClick={cliqueForm}>
           <label className="email-login">Email</label>
           <input
-          type="email"
-          placeholder="Digite seu e-mail"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
-          required />
-
-          <label className="email-senha">Senha</label>
-          <input 
-          type="password" 
-          placeholder="Digite sua senha" 
-          required 
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+            required
           />
 
-          <button onClick={submitLogin}>Login</button>
+          <label className="email-senha">Senha</label>
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            required
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+
+          <button className="buttonLogin" onClick={submitLogin}>Login</button>
 
           <a href="" className="esqueceu-senha" onClick={handleModal}>
             Esqueceu sua senha?
@@ -80,12 +89,7 @@ const Login = () => {
           <p className="criar-conta">
             É novo por aqui? <Link className="link-login" to="/registrar">Crie sua conta</Link>
           </p>
-        </form>
-      </div>
-
-      {modal && (
-        <div>
-           <ToastContainer
+          <ToastContainer
             position="top-right"
             autoClose={5000}
             hideProgressBar={false}
@@ -96,8 +100,24 @@ const Login = () => {
             draggable
             pauseOnHover
             theme="dark"
-            />
-            
+          />
+        </form>
+      </div>
+
+      {modal && (
+        <div>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
           <div className="fundo-modal" onClick={handleModal}></div>
           <div className="nova-senha">
             <h1>Esqueceu sua senha?</h1>
@@ -107,7 +127,7 @@ const Login = () => {
               value={input}
               onChange={(e) => setInput(e.target.value)}
             />
-            <button onClick={handlePassword}>Enviar</button>
+            <button className="buttonEnviar" onClick={handlePassword}>Enviar</button>
           </div>
         </div>
       )}
